6f6c8b365da575e311439015578e797c0f08aa42
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-08-18 12:10:19 +0300

    Add Question

---
+++ app/src/main/java/com/example/template/Question.kt
@@ -0,0 +1,4 @@
+package com.example.template
+
+data class Question(val text: String, val StoneButton: String, val scissorsButton: String,
+                    val PaperButton: String,  val count: Int, val resBackground: Int = 0)
