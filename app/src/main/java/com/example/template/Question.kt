ad8f380e8181d4cff086a6338258c2de7276d00e
Zabalkanskiy <azabalkanskij@hotmail.com>
2023-08-19 11:37:55 +0300

    change Question.kt

--- app/src/main/java/com/example/template/Question.kt
+++ app/src/main/java/com/example/template/Question.kt
@@ -1,4 +1,3 @@
 package com.example.template
 
-data class Question(val text: String, val StoneButton: String, val scissorsButton: String,
-                    val PaperButton: String,  val count: Int, val resBackground: Int = 0)
+data class Question(val resFootbalImage: Int)
